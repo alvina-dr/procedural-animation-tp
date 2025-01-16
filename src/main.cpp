2580b9803252f6688828259803df4cce41ad1dfc
JulesSarton <jjonchay12@gmail.com>
2025-01-16 01:57:00 +0100

    add particles

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "myviewer.cpp"
 #include "boids/boidsviewer.cpp"
+#include "boids/particlesviewer.cpp"
 
 int main(int argc, char** argv) {
-	BoidsViewer v;
+	ParticlesViewer v;
 	return v.run();
 }
